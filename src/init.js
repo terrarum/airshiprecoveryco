0ad73225d272bc5afaca6f6c6d46dde397b10a96
James Fidler <terrarum@gmail.com>
2015-07-04 00:16:43 +0100

    Crates picked and dropped.

--- src/init.js
+++ src/init.js
@@ -10,7 +10,7 @@ function getRandRange(min, max) {
 function preload() {
     game.load.image('airship', 'assets/images/airship.png');
     game.load.image('airpad', 'assets/images/airpad.png');
-    game.load.image('crate', 'assets/images/airpad.png');
+    game.load.image('crate', 'assets/images/crate.png');
 }
 
 function create() {
@@ -21,10 +21,17 @@ function create() {
     var startX = getRandRange(50, game.world.width - 50);
     var startY = getRandRange(50, game.world.height - 50);
 
+    // Crates.
+    crate = game.add.sprite(200, 200, 'crate');
+    game.physics.arcade.enable(crate);
+    crate.body.immovable = true;
+
     // Airpad.
     airpad = game.add.sprite(0, 0, 'airpad');
     airpad.x = startX - airpad.width / 2;
     airpad.y = startY - airpad.height / 2;
+    game.physics.arcade.enable(airpad);
+    airpad.body.immovable = true;
 
     // Player.
     player = game.add.sprite(0, 0, 'airship');
@@ -42,23 +49,70 @@ function create() {
     scoreText = game.add.text(16, 16, 'score: 0', {fontSize: '32px', fill: '#fff'});
 }
 
-function collectStar(player, star) {
+var carryingCrate = false;
+var hitTime = 0;
+var isFirst = true;
+function hitCrate(player, crate) {
+    // If this is the first hit, store the time.
+    if (isFirst) {
+        hitTime = Date.now();
+        isFirst = false;
+    }
 
-    // Removes the star from the screen
-    star.kill();
-
-    score += 10;
-    scoreText.text = 'Score: ' + score;
+    if (Date.now() - hitTime >= 2000) {
+        crate.x = -100;
+        crate.y = -100;
+        carryingCrate = true;
+    }
+}
+function missCrate(player, crate) {
+    isFirst = true;
 }
 
+// Drop crate at airpad.
+var isFirstAirpad = true;
+var landTime = 0;
+function dropCrate() {
+    // If this is the first hit, store the time.
+    if (isFirstAirpad) {
+        landTime = Date.now();
+        isFirstAirpad = false;
+    }
+
+    if (Date.now() - landTime >= 2000) {
+        crate.x = getRandRange(50, game.world.width - 50);;
+        crate.y = getRandRange(50, game.world.height - 50);;
+        carryingCrate = false;
+    }
+}
+function missPad(player, crate) {
+    isFirstAirpad = true;
+}
+
+
 function update() {
-    // Collide the player with the platforms.
+
+    // See if airship is over crate.
+    var didHitCrate = game.physics.arcade.overlap(player, crate, null, null, this);
+    if (didHitCrate) {
+        hitCrate(player, crate);
+    }
+    else {
+        missCrate(player, crate);
+    }
+
+    // If airship is over landing pad and carrying a crate.
+    var hitLandingPad = game.physics.arcade.overlap(player, airpad, null, null, this);
+    if (hitLandingPad && carryingCrate) {
+        dropCrate();
+    }
+    else if (!hitLandingPad && carryingCrate) {
+        missPad();
+    }
 
     var velocity = player.body.velocity;
 
     cursors = game.input.keyboard.createCursorKeys();
-
-    //  Reset the players velocity (movement)
 
     var moveRate = 2;
 
